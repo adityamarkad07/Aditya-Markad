872846e8db8e74061bdff884f1bf8df0b1b3810f
adityamarkad07 <adityamarkad07@gmail.com>
2025-08-17 15:28:18 +0530

    fix: changes

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,12 +1,12 @@
-import Footer from "@/components/Footer";
+import Footer from "@/pages/footer";
 import Navbar from "@/pages/Navbar";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar/>
-        <Outlet />
+      <Navbar />
+      <Outlet />
       <Footer />
     </div>
   );
